0b43ad4a8121c2d9ba7c510d0e6eca3ed1b8faef
goojin <goojin@localhost>
2023-03-28 13:00:19 +0900

    first commit 첫번째 커밋

---
+++ src/gittest_1/test_1.java
@@ -0,0 +1,10 @@
+package gittest_1;
+
+public class test_1 {
+
+	public static void main(String[] args) {
+		System.out.println("test_1");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author goojin
+ *
+ */
+module gittest {
+}
